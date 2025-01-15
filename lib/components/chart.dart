457f471a4cc83035630034c2b88756953c278c64
Thiago Costa <thiagoocdiniz@gmail.com>
2025-01-15 08:55:11 -0300

    final do chart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,22 +1,23 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'chart_bar.dart';
 
-class Chart extends StatelessWidget{
+class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
   const Chart(this.recentTransactions, {Key? key}) : super(key: key);
 
-  List<Map<String, Object>> get groupTransactions{
-    return List.generate(7, (index){
-      final weekDay =  DateTime.now().subtract(
+  List<Map<String, Object>> get groupTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
         Duration(days: index),
       );
       double totalSum = 0.0;
-      for(var i = 0; i < recentTransactions.length; i++){
+      for (var i = 0; i < recentTransactions.length; i++) {
         bool sameDay = recentTransactions[i].date.day == weekDay.day;
         bool sameMonth = recentTransactions[i].date.month == weekDay.month;
         bool sameYear = recentTransactions[i].date.year == weekDay.year;
-        if(sameDay && sameMonth && sameYear){
+        if (sameDay && sameMonth && sameYear) {
           totalSum += recentTransactions[i].value;
         }
       }
@@ -24,20 +25,36 @@ class Chart extends StatelessWidget{
         'day': DateFormat.E().format(weekDay)[0],
         'value': totalSum,
       };
-    }
-    );
+    });
+  }
+
+  double get _weekTotalValue {
+    return groupTransactions.fold(0.0, (sum, tr) {
+      return sum + (tr['value'] as double);
+    });
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     groupTransactions;
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
-      child: Row(
-        children: groupTransactions.map((tr){
-          return Text('${tr['day']}: ${tr['value']}');
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupTransactions.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: tr['day'] as String,
+                value: tr['value'] as double,
+                percentage: (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
